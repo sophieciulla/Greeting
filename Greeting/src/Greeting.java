17762e312ddfc666466b9f54904c9c9d0e541d8f
Sophie Ciulla <Sophie.Ciulla@MU-C1-09.mullen.pvt.k12.co.us>
2017-12-18 09:00:44 -0700

    initial

---
+++ Greeting/src/Greeting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+public class Greeting
+	{
+
+		public static void main(String[] args)
+			{
+				int d = 35;
+				for (int i = 5; i<36; i++)
+					{
+						
+						System.out.println(i + " " + d);
+						 d = d-1;
+						
+					}
+				
+			}
+
+	}
